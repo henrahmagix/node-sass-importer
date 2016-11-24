ae8d8b107991c8120444d142032e69557b9104cf
Henry Blyth <blyth.henry@gmail.com>
2016-11-24 18:15:24 +0000

    Ensure user sees error if thrown after done()

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 module.exports = function (importer) {
     return function (importPath, absFilepath, done) {
         var result;
-        var doneSafely;
+
+        var originalDone = done;
         var alreadyDone = false;
-        if (done) {
-            doneSafely = function () {
+        if (typeof done === 'function') {
+            done = function () {
                 alreadyDone = true;
-                done.apply(this, arguments);
+                originalDone.apply(this, arguments);
             };
         }
+
         try {
-            result = importer(importPath, absFilepath, doneSafely);
+            result = importer(importPath, absFilepath, done);
         } catch (error) {
             result = error;
+            if (alreadyDone) {
+                // Throw again to show the error to the user, because we cannot
+                // call done() again. This must not be done in sync mode, else
+                // the error is unhelpful: `Segmentation fault: 11` (it will be
+                // returned in sync mode and logged that way).
+                throw error;
+            }
         }
+
         if (done) {
             if (!alreadyDone) {
                 done(result);
