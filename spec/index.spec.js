4cda231ef23cc26d7f0e1675ec57338aecd4069b
Henry Blyth <blyth.henry@gmail.com>
2016-11-25 11:30:16 +0000

    Start unit tests

---
+++ spec/index.spec.js
@@ -0,0 +1,9 @@
+var sassImporter = require('..');
+
+describe('Main', function () {
+
+    it('should foo', function () {
+        expect(sassImporter).toEqual(jasmine.any(Function));
+    });
+
+});
